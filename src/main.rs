d282dcfdab831f58cbd25c73c2cd9897f8368f4a
frchandra <nismara.chandra@herdiputra.com>
2026-05-30 13:30:57 +0900

    print a promt and handle invalid command
    Signed-off-by: frchandra <nismara.chandra@herdiputra.com>

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,9 @@ use std::io::{self, Write};
 fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut command: String = String::new();
+    io::stdin().read_line(&mut command).unwrap();
+    println!("{}: command not found", command.trim());
+    io::stdout().flush().unwrap();
 }
