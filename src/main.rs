00103721a72556d39bbaac3e3d9e8a5cc4633537
frchandra <nismara.chandra@herdiputra.com>
2026-05-31 14:38:58 +0900

    exit trigger
    Signed-off-by: frchandra <nismara.chandra@herdiputra.com>

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,14 @@ fn main() {
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
+
         let mut command: String = String::new();
         io::stdin().read_line(&mut command).unwrap();
+
+        if command.trim() == "exit" {
+            break;
+        }
+
         println!("{}: command not found", command.trim());
         io::stdout().flush().unwrap();
     }
